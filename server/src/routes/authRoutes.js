752ab747471eafdecb3bed0d54a6aa77def797e2
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-03 18:54:30 +0530

    login route

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -35,4 +35,21 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(422).json({ error: "Invalid user" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 module.exports = router;
