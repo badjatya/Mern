4eedbc30c4aa1cbcda0f0e84959e1037a27aacfc
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-03 21:44:33 +0530

    Creating routes using react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 // dom
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 // Importing components
 import Navbar from "./components/Navbar";
 
 // Importing pages
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -15,6 +19,10 @@ const App = () => {
       <Navbar />
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </Switch>
     </>
   );
