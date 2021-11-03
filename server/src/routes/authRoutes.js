7be4436082d53cef46384f007ab09e2b9608ff45
Archit Badjatya <en19cs304010@medicaps.ac.in>
2021-11-03 13:31:48 +0530

    Register user created

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,6 +3,32 @@ const router = require("express").Router();
 // Model
 const User = require("../models/user");
 
+router.post("/register", async (req, res) => {
+  const { name, email, phone, work, password, confirmPassword } = req.body;
 
+  // if field doest not exit
+  if (!name || !email || !phone || !work || !password || !confirmPassword) {
+    return res.status(422).json({ error: "enter all filed" });
+  }
 
-module.exports = router
+  try {
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      res.status(422).json({ error: "User already exits" });
+    }
+
+    const user = new User({
+      name,
+      email,
+      phone,
+      work,
+      password,
+      confirmPassword,
+    });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {}
+});
+
+module.exports = router;
